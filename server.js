77cdedd21a73f26c85c172eab624cedc3e0d92ee
Fernando Gonçalves <jimmynando@gmail.com>
2019-08-07 17:44:05 -0300

    routes created

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const express = require('express');
-
+const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port:${PORT}`);
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("API running");
 });
 
-app.get('/', (req, res) => {
-    res.send('API running')
-})
+app.use("/api/auth", "./routes/api/auth");
+app.use("/api/posts", "./routes/api/posts");
+app.use("/api/profile", "./routes/api/profile");
+app.use("/api/users", "./routes/api/users");
+
+app.listen(PORT, () => {
+  console.log(`Server running on port:${PORT}`);
+});
