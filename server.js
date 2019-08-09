b7831aa6831d592f80924fc4d257aced70e83acf
Fernando Gonçalves <jimmynando@gmail.com>
2019-08-09 05:24:09 -0300

    rota de criação de usuário criada

--- server.js
+++ server.js
@@ -5,14 +5,16 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => {
   res.send("API running");
 });
 
-app.use("/api/auth", "./routes/api/auth");
-app.use("/api/posts", "./routes/api/posts");
-app.use("/api/profile", "./routes/api/profile");
-app.use("/api/users", "./routes/api/users");
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/users", require("./routes/api/users"));
 
 app.listen(PORT, () => {
   console.log(`Server running on port:${PORT}`);
